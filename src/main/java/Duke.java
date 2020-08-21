849c060782b5f20b73dbadf4db4f06ecfcf552bc
czlin98 <czlin98@gmail.com>
2020-08-21 20:30:59 +0800

    Add Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
     public static void main(String[] args) {
@@ -11,19 +12,51 @@ public class Duke {
                 "    ____________________________________________________________\n" +
                 "     Bye. Hope to see you again soon!\n" +
                 "    ____________________________________________________________\n";
+        String[] items = new String[100];
+        int itemCount = 0;
 
         System.out.println(greet);
+
         while(true) {
             Scanner in = new Scanner(System.in);
             String line = in.nextLine();
+
             if (line.equals("bye")) {
                 System.out.println(bye);
                 break;
             }
-            System.out.println(
-                    "    ____________________________________________________________\n" +
-                    "     " + line + "\n" +
-                    "    ____________________________________________________________\n");
+
+            else if (line.equals("list")) {
+                for (int i = 0; i < itemCount; i++) {
+                    if (i == 0) {
+                        System.out.println(
+                                "    ____________________________________________________________\n" +
+                                "     " + (i + 1) + ". " + items[i]
+                        );
+                    }
+                    else if (i == itemCount - 1) {
+                        System.out.println(
+                                "     " + (i + 1) + ". " + items[i] + "\n" +
+                                "    ____________________________________________________________\n"
+                        );
+                    }
+                    else {
+                        System.out.println("     " + (i + 1) + ". " + items[i]);
+                    }
+                }
+            }
+
+            else {
+                System.out.println(
+                        "    ____________________________________________________________\n" +
+                        "     added: " + line + "\n" +
+                        "    ____________________________________________________________\n"
+                );
+                for (String item : items) {
+                    items[itemCount] = line;
+                }
+                itemCount++;
+            }
         }
     }
 }
