fa3a803ab8bb687cdcc92b66f87461d815f72fb0
czlin98 <czlin98@gmail.com>
2020-08-21 20:00:39 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String greet =
                 "    ____________________________________________________________\n" +
                 "     Hello! I'm Duke\n" +
                 "     What can I do for you?\n" +
+                "    ____________________________________________________________\n";
+        String bye =
                 "    ____________________________________________________________\n" +
                 "     Bye. Hope to see you again soon!\n" +
                 "    ____________________________________________________________\n";
+
         System.out.println(greet);
+        while(true) {
+            Scanner in = new Scanner(System.in);
+            String line = in.nextLine();
+            if (line.equals("bye")) {
+                System.out.println(bye);
+                break;
+            }
+            System.out.println(
+                    "    ____________________________________________________________\n" +
+                    "     " + line + "\n" +
+                    "    ____________________________________________________________\n");
+        }
     }
 }
